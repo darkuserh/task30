70645622206f81668063374443ffaebd57fd1232
vysotsk11yu7iy <vysotsk11yu7iy@gmail.com>
2023-07-24 01:02:23 +0300

    do a new task

--- my-script.js
+++ my-script.js
@@ -1,5 +1,32 @@
-let cities=['Lviv','Kyiv','Stryi'];
-cities.push(prompt('Введіть місто:'))
-cities.pop()
-alert('Дане місто відсутнє')
-console.log(cities)
+
+let values = [10, 20, 5, 7, 11, 8, 125, 4, 8, 3, 23];
+let oddValues = [];
+let evenValues = [];
+let oddValuesPosition = 0;
+let evenValuesPosition = 0;
+
+for (let value of values)
+{
+    if (value % 2 == 0)
+    {
+        evenValues.push(value) ;
+       
+    }
+    else
+    {
+        oddValues.push(value);
+     
+    }
+}
+
+console.log("Непарні значення");
+for (let oddVal of oddValues)
+{
+    console.log(oddVal);
+}
+
+console.log("Парні значення");
+for (let evenVal of evenValues)
+{
+    console.log(evenVal);
+}
