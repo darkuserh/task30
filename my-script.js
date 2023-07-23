29a9ef65d78b529eb9173f2f965e8a7c1d3814a6
vysotsk11yu7iy <vysotsk11yu7iy@gmail.com>
2023-07-23 23:46:22 +0300

    new task

--- my-script.js
+++ my-script.js
@@ -1,17 +1,14 @@
-let values=[10,20,30,40,50,60]
-let max=Number.MIN_VALUE;
-let min=Number.MAX_VALUE;
-let sum=0;
-for(let i=0;i<values.length;i++){
-    const element=values[i];
-    sum+=element
-    if(element>max){
-        max=element;
-    } if(element<min){
-        min=element;
+
+let values = [10, 20, 4, 6, 11, 9, 125, 1, 8, 0, 3];
+let max = Number.MIN_VALUE;
+
+for (let value of values) { 
+{
+    const element = value;
+    if(element > max)
+    {
+        max = element;
     }
-    
 }
-console.log(sum);
-console.log(max)
-console.log(min)                                                                                                         
+}
+console.log("Максимальне значення массиву - " + max);
