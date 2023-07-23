3d95525cbb18d59c9a476537d2358545c01ef1dd
vysotsk11yu7iy <vysotsk11yu7iy@gmail.com>
2023-07-24 00:09:49 +0300

    task yet

--- my-script.js
+++ my-script.js
@@ -1,14 +1,9 @@
+let values = [10, 20, -2, 6, 11, -10, 125, -3, 8, 0, 3];
 
-let values = [10, 20, 4, 6, 11, 9, 125, 1, 8, 0, 3];
-let max = Number.MIN_VALUE;
-
-for (let value of values) { 
-{
-    const element = value;
-    if(element > max)
-    {
-        max = element;
+for(let i=0;i<values.length;i++){
+    if(values[i]<0){
+    values[i]=Number(String(values[i]).split('-')[0]);
+    
     }
 }
-}
-console.log("Максимальне значення массиву - " + max);
+console.log(values);
