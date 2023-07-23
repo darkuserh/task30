1154e17f15c4dc8f5c41c97061eb0a468aa8c5dd
vysotsk11yu7iy <vysotsk11yu7iy@gmail.com>
2023-07-24 00:56:11 +0300

    add new task

--- my-script.js
+++ my-script.js
@@ -1,13 +1,5 @@
-let values = [-1, -5, -6, -3, -10, -5, 3, 4, -1, -8, 1, -10, 3];
-let positiv=[];
-for(let i=0;i<values.length;i++){
-    let value=values[i];
-    
-    if(value > 0){
-   
-     positiv.push(value);
-    
-    }
-  
-}
-console.log(positiv[0]);
+let cities=['Lviv','Kyiv','Stryi'];
+cities.push(prompt('Введіть місто:'))
+cities.pop()
+alert('Дане місто відсутнє')
+console.log(cities)
