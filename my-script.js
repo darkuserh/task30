52695a22b6e390c5d78d0fa0c3ebcca3ed94c43b
vysotsk11yu7iy <vysotsk11yu7iy@gmail.com>
2023-07-24 21:32:24 +0300

    do a new task

--- my-script.js
+++ my-script.js
@@ -1,18 +1,7 @@
-let values = [];
-let sum=0;
-while(true){
- let input=prompt('Введіть дане число')
- if(input === 'exit') break;
- let value = Number(input)
- if(isNaN(value)){
-    alert('Please enter')
- }else{
-    values.push(value)
- }
-  sum=0;
- for(let i=0; i<values.length; i++){
-sum+=values[i] 
+function min(a,b){
+    if(a < b){
+        return a;
+    } 
 }
- 
-} 
-alert('Дані значення сумуються'+sum)   
+let res=min(4,5)
+console.log(res)
