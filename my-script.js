dac8ce2b508aee9d141754e323d117b658a9aef6
vysotsk11yu7iy <vysotsk11yu7iy@gmail.com>
2023-07-25 20:21:48 +0300

    do a new task

--- my-script.js
+++ my-script.js
@@ -1,6 +1,38 @@
-let values = [10, 20, -2, 6, 11, -10, 125, -3, 8, 0, 3];
-for( i=0;i<values.length;i++){
- let value=values.filter((i)=>{return i>=0})
- console.log(value);
-  
-}
+let productQuantity=[];
+        // назва товарів
+        let names = [];
+        names[0] = "Папір офісний А4, 80 г/м2, 500 л";
+        names[1] = "Біндер для паперу 51 мм";
+        names[2] = "Ручка кулькова синя";
+
+        // ціни на товар
+        let prices = [];
+        prices[0] = 280.25; // Папір офісний А4, 80 г/м2, 500 л
+        prices[1] = 56;     // Біндер для паперу 51 мм
+        prices[2] = 12.50;  // Ручка кулькова синя
+    
+        let count = [];
+        count[0] = 2;   // Папір офісний А4, 80 г/м2, 500 л
+        count[1] = 10;  // Біндер для паперу 51 мм
+        count[2] = 10;  // Ручка кулькова синя
+
+        for(let i = 0; i < count.length; i++){
+            const Count=count[i];
+            const price=prices[i];
+        
+          productQuantity[i]=+prompt(`Введіть кількість продуктів ${Count} та ціна ${price}`,0)
+        }
+        let totalPrice=0;
+        let totaItems1=0;
+        let totaItems2=0;
+        let totaItems3=0;
+        for(let i = 0; i < productQuantity.length; i++){
+            totalPrice+=productQuantity[i]*prices[i]
+            totaItems1+=productQuantity[0]*prices[0]
+            totaItems2+=productQuantity[1]*prices[1]
+            totaItems3+=productQuantity[2]*prices[2]
+          productQuantity[i]=+prompt(`Загальна сума ${totalPrice} `,0)
+          productQuantity[i]=+prompt(` сума  1 товару ${totaItems1} `,0)
+          productQuantity[i]=+prompt(` сума  2 товару ${totaItems2} `,0)
+          productQuantity[i]=+prompt(` сума  3 товару ${totaItems3} `,0)
+        }
