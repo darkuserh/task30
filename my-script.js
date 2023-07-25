861d4c78b6c9070dc9f18588d0104e77fbe11598
vysotsk11yu7iy <vysotsk11yu7iy@gmail.com>
2023-07-26 00:08:04 +0300

    do a new task

--- my-script.js
+++ my-script.js
@@ -1,14 +1,44 @@
-function test(value){
 
-if(value<1){
-  return;
-}
-console.log(value);
+        // назва товарів
+        let names = [];
+        names[0] = "Папір офісний А4, 80 г/м2, 500 л";
+        names[1] = "біндери для паперу 51 мм";
+        names[2] = "Ручка кулькова синя";
 
-test(value-1);
+        // ціна на товар
+        let prices = [];
+        prices[0] = 280.25; // Папір офісний А4, 80 г/м2, 500 л
+        prices[1] = 56;     // біндери для паперу 51 мм
+        prices[2] = 12.50;  // Ручка кулькова синя
+
+        let count = [];
 
 
-}
 
+        let initCountArray =  () => {
+            for (let i = 0; i < names.length; i++) {
+                const productName = names[i];
+                let counttoBuy = +prompt("Вкажіть кількість для " + productName);
 
-test(+prompt('Введіть число ',0))
+                if (isNaN(counttoBuy))
+                    counttoBuy = 0;
+
+                count.push(counttoBuy);
+            }
+        }
+
+        let calculateSum  =  () => {
+            let totalSum = 0;
+            for (let i = 0; i < names.length; i++) {
+                const productName = names[i];
+                const productPrice = prices[i];
+                const boughtCount = count[i];
+
+                let price = productPrice * boughtCount;
+                totalSum += price;
+                console.log(`Придбано ${productName} на суму ${price}`);
+            }
+            console.log("Всього продуктів придбано на " + totalSum);
+        }
+        initCountArray();
+        calculateSum();
