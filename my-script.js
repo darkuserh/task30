a10402a14ed2712c432c2bd35492cb9db61d1d79
vysotsk11yu7iy <vysotsk11yu7iy@gmail.com>
2023-07-27 01:46:01 +0300

    do a new task

--- my-script.js
+++ my-script.js
@@ -1,14 +1,13 @@
-const arr=[1,2,3,4,5,6,7,7,7,7]
-const makeUniq=(arr)=>{
-    return arr.filter((el,id)=>{return arr.indexOf(el) === id})
-}
-function uniqValues(arr){
-    if(makeUniq){
-        return null;
-    }else{
-       
-        return arr;
-    }
-}
-const UniqCode=uniqValues(arr);
-console.log(UniqCode);
+let user = {
+    name: "admin",
+    email: "admin@example.com"
+};
+
+console.log(user.name);
+console.log(user.email);
+
+user.email = "admin@itvdn.com";
+
+console.log(user.email);
+
+
